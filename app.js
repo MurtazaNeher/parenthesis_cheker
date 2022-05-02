f554712b55137e902842528b7bc61b8ee1d658f1
MurtazaNeher <96943172+MurtazaNeher@users.noreply.github.com>
2022-05-02 11:54:09 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const inputBox = document.querySelector("#input-box");
+const checkBtn = document.querySelector("#check-btn");
+const outputBox = document.querySelector("#output-result");
+
+
+
+
+
+
+
+function checkClickHandler() {
+    console.log("clicked");
+    let inputStr = inputBox.value;
+
+
+}
+
+
+checkBtn.addEventListener("click", checkClickHandler);
